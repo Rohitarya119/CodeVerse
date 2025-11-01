dd490a3f115ffb04eb8c2253c010839f2f4437b0
rohitarya119 <raviarya201016@gmail.com>
2025-11-01 22:45:11 +0530

    Initial commit: Add Spring Boot backend with JWT auth

---
+++ Frontend/Codeverse-main/codeverse/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
